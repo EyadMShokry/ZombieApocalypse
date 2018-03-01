2734b5f62ee6027ab97df31202d67b91a030ee43
Andrew Nagyeb <andrewnajeeb@fci.helwan.edu.eg>
2018-03-01 23:52:16 +0200

    Adding Torch Prefab, and SoundManager game object
    Adding Torch for the player which has a battery that decreases automatically as long as the torch is switched on, however, battery screen design is not made yet. Moreover, SoundManager gameobject is used to map all of our game sounds to it, it has an attached SoundManagerScript which we will use in our scripts to play sounds, rather than creating an audio source for each object we use.

---
+++ Assets/scripts/SoundManagerScript.cs
@@ -0,0 +1,34 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+	public static AudioClip flashLightSwitchOnSound, flashLightSwitchOffSound;
+	static AudioSource audioSrc;
+	// Use this for initialization
+	void Start ()
+	{
+		flashLightSwitchOnSound = Resources.Load<AudioClip> ("sounds/torch_sound/large_flashlight_switched_on");
+		flashLightSwitchOffSound = Resources.Load<AudioClip> ("sounds/torch_sound/large_flashlight_switched_off");
+		audioSrc = GetComponent<AudioSource> (); 
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	public static void PlaySound (string clip)
+	{
+		switch (clip) {
+		case "FlashlightSwitchOn":
+			audioSrc.PlayOneShot (flashLightSwitchOnSound);
+			break;
+		case "FlashlightSwitchOff":
+			audioSrc.PlayOneShot (flashLightSwitchOffSound);
+			break;
+		}
+	}
+}
---
+++ Assets/scripts/flashlight_script/FlashlightScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashlightScript : MonoBehaviour {
+	// Public variables to modify the place of the battery rect.
+	public float a;
+	public float b;
+	public float c;
+	public float d;
+	// Battery Integer Level.
+	public int batLevel;
+	// Spotlight Object.
+	public Light FLight;
+	// Boolean variable to describe the flashlight state.
+	public bool isOn;
+	// timer that automatically decreases the battery level.
+	public float timer;
+	// Charging bars tracker flag
+	public int charging_bars_tracker;
+	public GameObject[] charging_bars;
+	void Start()
+	{
+		// Catching the component which is in the children of this.
+		FLight = GetComponentInChildren<Light> ();
+		// Defining the light as spot.
+		FLight.type = LightType.Spot;
+		// Battery level initialized with 15.
+		batLevel = 15;
+		minusBat();
+		isOn = false;
+		charging_bars_tracker = 0;
+		charging_bars = new GameObject[5];
+		for (int i = 0; i < 5; i++) {
+			charging_bars [i] = GameObject.Find ("Cube " + "(" + (i+1) + ")");
+
+		}
+	}
+
+	void minusBat()
+	{
+		if (isOn)
+		{
+			batLevel -= 1;
+			FLight.intensity -= 1;
+			charging_bars_tracker += 1;
+		}
+	}
+
+	void Update()
+	{
+
+		if(timer >= 0)
+		{
+			if (isOn)
+			{
+				timer -= Time.deltaTime;
+			}
+		}
+
+		if (timer <= 0)
+		{
+			timer = 5;
+			minusBat();
+		}
+
+		if (Input.GetKeyUp(KeyCode.F))
+		{
+			FLight.enabled = !FLight.enabled;
+
+			if (!isOn)
+			{
+				isOn = true;
+				SoundManagerScript.PlaySound ("FlashlightSwitchOn");
+
+			}
+			else
+			{
+				isOn = false;
+				SoundManagerScript.PlaySound ("FlashlightSwitchOff");
+			}
+
+		}
+
+		if(batLevel == 0)
+		{
+			batLevel = 0;
+			FLight.enabled = false;
+			isOn = false;
+		}
+
+	}
+		
+}
