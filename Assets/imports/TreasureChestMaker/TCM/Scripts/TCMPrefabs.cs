d54692600f14d41d26bfc49d7740e17783bdc74f
SalmaDoma <salma-922@hotmail.com>
2018-03-05 20:44:58 +0200

    Enhancing the enviroment structure

--- Assets/imports/TreasureChestMaker/TCM/Scripts/TCMPrefabs.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace TCM {
-	public class TCMPrefabs : MonoBehaviour {
-		public GameObject chestSmall,chestMedium,chestLarge;
-		public Texture borderWood, borderMetal, baseWood, baseMetal;
-	}
-}
