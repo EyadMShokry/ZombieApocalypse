62db66dd2dabf14401b7c72a96c70eb6cd38f7de
ahmedsaeedmedo <ahmedsaeedmedo6@gmail.com>
2018-03-08 08:43:45 +0200

    add hand
    hand and animaton

---
+++ Assets/FPS Kit By (HDGameP)/My Kit/Acr/Scripts/WeaponTest.js
@@ -0,0 +1,50 @@
+﻿var drawAnim : String = "Draw";
+var fireLeftAnim : String = "Fire";
+var reloadAnim : String = "Reload";
+var animationGO : GameObject;
+ 
+private var drawWeapon : boolean = false;
+private var reloading : boolean = false;
+ 
+function Start (){
+DrawWeapon();
+}
+ 
+function Update (){
+ 
+    if(Input.GetButtonDown ("Fire1") && reloading == false && drawWeapon == false){
+        Fire();
+        }
+       
+        if (Input.GetKeyDown ("r") && reloading == false && drawWeapon == false){
+    Reloading();
+        }
+       
+        if (Input.GetKeyDown ("1") && reloading == false){
+        DrawWeapon();
+        }      
+}
+ 
+function Fire(){
+    animationGO.GetComponent.<Animation>().CrossFadeQueued(fireLeftAnim, 0.08, QueueMode.PlayNow);
+}
+ 
+function DrawWeapon() {
+  if(drawWeapon)
+    return;
+       
+        animationGO.GetComponent.<Animation>().Play(drawAnim);
+        drawWeapon = true;
+        yield WaitForSeconds(0.6);
+        drawWeapon = false;
+       
+}
+ 
+function Reloading(){
+     if(reloading) return;
+   
+        animationGO.GetComponent.<Animation>().Play(reloadAnim);
+        reloading = true;
+        yield WaitForSeconds(2.0);
+        reloading = false;
+}
---
+++ Assets/scripts/enviroment_script/conroller_scripts/weapons/WeaponAnimation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponAnimation : MonoBehaviour
+{
+    public Animation anim;
+    public AnimationClip fire;
+    public AnimationClip reload;
+    public AnimationClip draw;
+    public KeyCode reloadKey = KeyCode.R;
+    public KeyCode fireKey = KeyCode.Mouse0;
+    public KeyCode drawKey = KeyCode.D;
+
+    void Awake()
+    {
+        if (fire == null) Debug.LogError("Please assign a fire aimation in the inspector!");
+        if (reload == null) Debug.LogError("Please assign a reload animation in the inspector!");
+        if (draw == null) Debug.LogError("Please assign a draw animation in the inspector!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(reloadKey) && !anim.IsPlaying(fire.name)&&!anim.IsPlaying(draw.name))
+        {
+            if (reload != null) anim.Play(reload.name);
+        }
+		else if (Input.GetMouseButton(0) && !anim.IsPlaying(reload.name)&&!anim.IsPlaying(draw.name))
+        {
+            if (fire != null) anim.Play(fire.name);
+        }
+		else if (Input.GetKeyDown(drawKey) &&!anim.IsPlaying(fire.name)&&!anim.IsPlaying(reload.name))
+        {
+            if (draw != null) anim.Play(draw.name);
+        }
+    }
+}
