fdd49147bb4d5eafacddad6ad41870056e46491c
Andrew Nagyeb <andrewnajeeb@fci.helwan.edu.eg>
2018-03-26 20:10:27 +0200

    Adding Biting Animation and Camera Shaking Effect

---
+++ Assets/scripts/DifficultyControlScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DifficulityControlScript : MonoBehaviour
+{
+	//Following variables are concerned with camera shaking effect.
+	private static float cameraShakingPower = 0.7f;
+	private static float cameraShakingDuration = 1.0f;
+	private static float cameraShakingSlowDownAmount = 1.0f;
+	// ------------------------------
+
+
+	public static float CameraShakingPower {
+		get {
+			return cameraShakingPower;
+		}
+		set {
+			cameraShakingPower = value;
+		}
+	}
+
+	public static float CameraShakingDuration {
+		get {
+			return cameraShakingDuration;
+		}
+		set {
+			cameraShakingDuration = value;
+		}
+	}
+
+	public static float CameraShakingSlowDownAmount {
+		get {
+			return cameraShakingSlowDownAmount;
+		}
+		set {
+			cameraShakingSlowDownAmount = value;
+		}
+	}
+
+
+}
