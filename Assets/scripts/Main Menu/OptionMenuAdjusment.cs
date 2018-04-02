e53e769d19ebe957440837923f640e81fe9bb9bf
MinaMofreh <minamofreh35@gmail.com>
2018-04-02 14:53:38 +0200

    Graphics Quality and full/mini Screen adjusment

---
+++ Assets/scripts/Main Menu/OptionMenuAdjusment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionMenuAdjusment : MonoBehaviour {
+
+	public void setGraphicsQuality (int qualityIndex){
+		QualitySettings.SetQualityLevel(qualityIndex);
+	}
+
+	public void setFullScreen (bool isFullScreen){
+		Screen.fullScreen = isFullScreen;
+	}
+}
