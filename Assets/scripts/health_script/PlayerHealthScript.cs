09df63bf0a24c4f8c5c327d7a0cc6f2f763157f2
SalmaDoma <salma-922@hotmail.com>
2018-04-04 01:25:30 +0200

    Zombie attack effects

--- Assets/scripts/health_script/PlayerHealthScript.cs
+++ Assets/scripts/health_script/PlayerHealthScript.cs
@@ -1,143 +1,125 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 
 public class PlayerHealthScript : MonoBehaviour
+{
+    static PlayerHealthScript instance;
+    public static PlayerHealthScript Instance { get { return instance; } }
+    //bool canTakeDamage = true;
+
+    public int maxHealth = 100;
+    float currentHealth = 100;
+    public SimpleHealthBarScript healthBar;
+    //public SimpleHealthBar shieldBar;
+
+    void Awake()
     {
-        static PlayerHealthScript instance;
-        public static PlayerHealthScript Instance { get { return instance; } }
-        //bool canTakeDamage = true;
+        // If the instance variable is already assigned, then there are multiple player health scripts in the scene. Inform the user.
+        if (instance != null)
+            Debug.LogError("There are multiple instances of the Player Health script. Assigning the most recent one to Instance.");
 
-        public int maxHealth = 100;
-        float currentHealth = 100;
-        //public float invulnerabilityTime = 0.5f;
-
-        //float currentShield = 0;
-        //public int maxShield = 25;
-        //float regenShieldTimer = 0.0f;
-        //public float regenShieldTimerMax = 1.0f;
-
-        //public GameObject explosionParticles;
-
-        public SimpleHealthBarScript healthBar;
-        //public SimpleHealthBar shieldBar;
-
-
-        void Awake()
-        {
-            // If the instance variable is already assigned, then there are multiple player health scripts in the scene. Inform the user.
-            if (instance != null)
-                Debug.LogError("There are multiple instances of the Player Health script. Assigning the most recent one to Instance.");
-
-            // Assign the instance variable as the Player Health script on this object.
-            instance = GetComponent<PlayerHealthScript>();
-        }
-
-        void Start()
-        {
-            // Set the current health and shield to max values.
-            currentHealth = maxHealth;
-            //currentShield = maxShield;
-
-            // Update the Simple Health Bar with the updated values of Health and Shield.
-            healthBar.UpdateBar(currentHealth, maxHealth);
-            //shieldBar.UpdateBar( currentShield, maxShield );
-        }
-
-        /*void Update ()
-		{
-			// If the shield is less than max, and the regen cooldown is not in effect...
-			if( currentShield < maxShield && regenShieldTimer <= 0 )
-			{
-				// Increase the shield.
-				currentShield += Time.deltaTime * 5;
-
-				// Update the Simple Health Bar with the new Shield values.
-				shieldBar.UpdateBar( currentShield, maxShield );
-			}
-
-			// If the shield regen timer is greater than zero, then decrease the timer.
-			if( regenShieldTimer > 0 )
-				regenShieldTimer -= Time.deltaTime;
-		}*/
-
-        public void HealPlayer(int health)
-        {
-            // Increase the current health by 25%.
-            currentHealth += health;
-
-            // If the current health is greater than max, then set it to max.
-            if (currentHealth > maxHealth)
-                currentHealth = maxHealth;
-
-            // Update the Simple Health Bar with the new Health values.
-            healthBar.UpdateBar(currentHealth, maxHealth);
-        }
-
-        public void TakeDamage(int damage)
-        {
-
-		if (currentHealth > 0)
-   {
-      currentHealth -= damage;
-			if (currentHealth > 10)
-			{
-				SoundManagerScript.PlaySound ("characterHurt");
-			}
-
-			if (currentHealth <= 10 && currentHealth != 0)
-			{
-				SoundManagerScript.PlaySound ("characterHurt");
-				SoundManagerScript.PlaySound ("characterHeavyBreathing");
-			}
-			if (currentHealth == 0)
-			{
-				SoundManagerScript.PlaySound ("characterDeath");
-			}
-
-
-	}
-	
-
-
-    else
-    {
-                // Set the current health to zero.
-                currentHealth = 0;
-
-
-                // Run the Death function since the player has died.
-                //Death();
+        // Assign the instance variable as the Player Health script on this object.
+        instance = GetComponent<PlayerHealthScript>();
     }
-    
 
-            // Update the Health and Shield status bars.
-            healthBar.UpdateBar(currentHealth, maxHealth);
+    void Start()
+    {
+        // Set the current health and shield to max values.
+        currentHealth = maxHealth;
 
+        // Update the Simple Health Bar with the updated values of Health and Shield.
+        healthBar.UpdateBar(currentHealth, maxHealth);
+    }
+
+    public void HealPlayer(int health)
+    {
+        // Increase the current health by 25%.
+        currentHealth += health;
+
+        // If the current health is greater than max, then set it to max.
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        // Update the Simple Health Bar with the new Health values.
+        healthBar.UpdateBar(currentHealth, maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+        if (currentHealth > 0)
+        {
+            currentHealth -= damage;
+            if (currentHealth > 10)
+            {
+                SoundManagerScript.PlaySound("characterHurt");
+            }
+
+            if (currentHealth <= 10 && currentHealth != 0)
+            {
+                SoundManagerScript.PlaySound("characterHurt");
+                SoundManagerScript.PlaySound("characterHeavyBreathing");
+            }
+            if (currentHealth == 0)
+            {
+                SoundManagerScript.PlaySound("characterDeath");
+            }
         }
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.CompareTag("healthLarge"))
-		{
-			other.gameObject.SetActive(false);
-			HealPlayer(70);
-		}
-		else if (other.gameObject.CompareTag("healthMid"))
-		{
-			other.gameObject.SetActive(false);
-			HealPlayer(50);
-		}
-		else if (other.gameObject.CompareTag("healthSmall"))
-		{
-			other.gameObject.SetActive(false);
-			HealPlayer(20);
-		}
-		else if(other.gameObject.CompareTag("damageKits"))
-		{
-			other.gameObject.SetActive(false);
-			TakeDamage(10);
-		}
-	}
+        else
+        {
+            // Set the current health to zero.
+            currentHealth = 0;
+
+            // Run the Death function since the player has died.
+            Death();
+        }
+
+        // Update the Health and Shield status bars.
+        healthBar.UpdateBar(currentHealth, maxHealth);
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("healthLarge"))
+        {
+            other.gameObject.SetActive(false);
+            HealPlayer(70);
+        }
+        if (other.gameObject.CompareTag("healthMid"))
+        {
+            other.gameObject.SetActive(false);
+            HealPlayer(50);
+        }
+        if (other.gameObject.CompareTag("healthSmall"))
+        {
+            other.gameObject.SetActive(false);
+            HealPlayer(20);
+        }
+
+        if (other.gameObject.CompareTag("damageKits"))
+        {
+            other.gameObject.SetActive(false);
+            TakeDamage(10);
+        }
+        if (other.gameObject.CompareTag("zombieGirl"))
+        {
+            TakeDamage((int)Math.Ceiling(Time.timeScale = 0.1f));
+        }
+        if (other.gameObject.CompareTag("zombiePolice"))
+        {
+            TakeDamage((int)Math.Ceiling(Time.timeScale = 0.2f));
+        }
+     
+
+    }
+
+    public void Death()
+    {
+
+    }
 
 }
