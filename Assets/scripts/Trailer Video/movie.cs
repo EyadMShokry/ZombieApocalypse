91a76e47179d30bc6c63646acf8f8975b57e9dd5
MaiMokhtar <36748060+MaiMokhtar@users.noreply.github.com>
2018-04-09 15:00:19 +0200

    added video game trailer

---
+++ Assets/scripts/Trailer Video/movie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movie : MonoBehaviour {
+
+    public Canvas startmenu;
+    public Canvas videoPlayer;
+
+   //private MovieTexture video;
+
+    // Use this for initialization
+    void Start () {
+        startmenu.enabled =false;
+        //((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+            
+       /* if ( video.isPlaying== false) {
+            videoPlayer.enabled = false;
+            startmenu.enabled = true;
+        }*/
+           
+		
+	}
+}
---
+++ Assets/scripts/Trailer Video/skipVideo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skipVideo : MonoBehaviour {
+
+    public Canvas startmenu;
+    public Canvas videoPlayer;
+
+
+
+    public void onClick() {
+    
+        videoPlayer.enabled = false;
+        startmenu.enabled = true;
+    
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            videoPlayer.enabled = false;
+            startmenu.enabled = true;
+        }
+
+    }
+}
