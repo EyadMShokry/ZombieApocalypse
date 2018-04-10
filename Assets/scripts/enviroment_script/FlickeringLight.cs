404d356c92137ed5d59a10837dbd4e06e39617e8
Sheko00o <ahmed.sherif.fcih@gmail.com>
2018-04-10 22:16:50 +0200

    Enhancing Enviroment
    Removed unsed assets, materials, prefabs, etc...
    
    Fixed many aligments of walls, floors and ceilings together
    
    Removed road to elevator
    
    Removed some Objects like skull, teeth, bed, barrel, etc...
    
    Changed and enhanced Texture of the ground except for the bathroom
    
    Enhanced lightining effects to be more scarry
    
    Added Camera post processing effects (Antialiasing, Bloom, motion blur, etc...)

---
+++ Assets/scripts/enviroment_script/FlickeringLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+
+	public float minWaitTime;
+	public float maxWaitTime;
+
+	void Start () {
+		StartCoroutine(Flashing());
+	}
+
+	IEnumerator Flashing ()
+	{
+		while (true)
+		{
+			/*yield return new WaitForSeconds(Random.Range(minWaitTime,maxWaitTime));
+			gameObject.SetActive(!gameObject.activeInHierarchy);*/
+			Debug.Log ("Hello");
+		}
+	}
+}
