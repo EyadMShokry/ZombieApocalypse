1aa8f9de158d741bb62fe9fd3f91f0cea9d594b3
mena mohamed <36733242+MenaAllahMohamed@users.noreply.github.com>
2018-04-16 22:55:11 +0200

    Fixed mainMenu and add pauseMenu

---
+++ Assets/scripts/Main Menu/OptionMenu_in_PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionMenu_in_PauseMenu : MonoBehaviour
+{
+    Text Precentage_text;
+    public GameObject pauseMenu;
+    public Canvas optionMenu;
+
+    void start()
+    {
+        Precentage_text = GetComponent<Text>();
+        Debug.LogError(Precentage_text);
+    }
+
+    public void back_to_pausemenu()
+    {
+        optionMenu.enabled = false;
+        pauseMenu.SetActive(true);
+    }
+
+    public void text_update(float value)
+    {
+
+        Precentage_text.text = Mathf.RoundToInt(value * 100) + "%";
+    }
+
+
+}
---
+++ Assets/scripts/Main Menu/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false ;
+
+    public GameObject pauseMenu;
+    public Canvas optionMenu;
+
+    private void Start()
+    {
+        optionMenu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }	
+	}
+
+   public void Resume(){
+
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+   }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);   //enable pause menu
+        Time.timeScale = 0f;    //freeze the game
+        GameIsPaused = true;
+    }
+
+    public void option()
+    {
+        pauseMenu.SetActive(false);
+        optionMenu.enabled=true;
+    }
+}
