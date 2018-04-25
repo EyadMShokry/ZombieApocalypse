100ac7e595dc2c2f70e4a1e53a3e7d429adbdfef
mena mohamed <36733242+MenaAllahMohamed@users.noreply.github.com>
2018-04-25 11:51:26 +0200

    Update MainMenu

--- Assets/scripts/Main Menu/QuitMenu_in_pauseMenu.cs
+++ Assets/scripts/Main Menu/QuitMenu_in_pauseMenu.cs
@@ -5,40 +5,7 @@ using UnityEngine.UI;
 
 public class QuitMenu_in_pauseMenu : MonoBehaviour {
 
-    public Canvas quitMenu;
-    public Canvas pauseMenu;
-    public Button resumeText;
-    public Button quitText;
-
-    Animator anim;
-
-    // Use this for initialization
-    void Start () {
-        quitMenu = quitMenu.GetComponent<Canvas>();
-        pauseMenu = pauseMenu.GetComponent<Canvas>();
-        quitText = quitText.GetComponent<Button>();
-        resumeText = resumeText.GetComponent<Button>();
-        quitMenu.enabled = false;
-        anim = GetComponent<Animator>();
-    }
-
     public void ExitPress()  //the button quit
-    {
-        quitMenu.enabled = true;
-        resumeText.enabled = false;
-        quitText.enabled = false;
-
-        anim.Play("buttonTweenAnims_on");
-    }
-
-    public void NoPress()  //the button No
-    {
-        quitMenu.enabled = false;
-        resumeText.enabled = true;
-        quitText.enabled = true;
-    }
-
-    public void QuitGame()  //the button yes
     {
         Application.Quit();
         Debug.Log("Game is exiting"); //Just to make sure its working
