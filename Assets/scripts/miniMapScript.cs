854543838f290cf15ec37549fc7e8cdc60ef734b
MarcEssam <36736446+MarcEssam@users.noreply.github.com>
2018-04-27 19:09:34 +0200

    adding minimap to game
    mapping the player's  position and mapping the environment

---
+++ Assets/scripts/miniMapScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miniMapScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform player;
+	void LateUpdate()
+	{
+		Vector3 newPosition = player.position;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+	}
+}
