2313f24746c7800b05386d8c2cc735a595e72ac0
MaiMokhtar <36748060+MaiMokhtar@users.noreply.github.com>
2018-04-28 20:42:53 +0200

    added audio for video
    stop the audio for main menu when video is playing then playing when the video finished

--- Assets/scripts/Main Menu/soundController.cs
+++ Assets/scripts/Main Menu/soundController.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class soundController : MonoBehaviour {
-	public AudioSource audio;
+public class soundController : MonoBehaviour
+{
+    public AudioSource audio;
+    public Canvas startmenu;
+    public Canvas videoPlayer;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Scene scene = SceneManager.GetActiveScene ();
-		if (scene.name != "MainMenu")
-			audio.clip.UnloadAudioData ();
-	}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name != "MainMenu" || videoPlayer.gameObject.activeSelf)
+        {
+            audio.clip.UnloadAudioData();
+        }
+        else if (scene.name == "MainMenu" && startmenu.gameObject.activeSelf)
+        {
+
+            SoundManagerScript.PlaySound("startingMusic");
+        }
+    }
 }
