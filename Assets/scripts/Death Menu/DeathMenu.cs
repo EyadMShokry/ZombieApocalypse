ed44a56f65b7da5dcc8ae2fa8a301f838cd5dfdc
mena mohamed <36733242+MenaAllahMohamed@users.noreply.github.com>
2018-04-29 01:18:45 +0200

    Update Death Menu

---
+++ Assets/scripts/Death Menu/DeathMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour {
+
+	public void retry()
+    {
+        SceneManager.LoadScene("cityEnvironment");
+    }
+
+    public void quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
